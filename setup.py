5afbe6de215852bcf6c50972787e2d71226a3e79
Neil <mistersheik@gmail.com>
2017-10-26 02:50:43 -0400

    Bumped version.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'ipromise',
     packages = ['ipromise'],
-    version = '0.1',
+    version = '0.2',
     description = 'A Python base class that provides various decorators for specifying promises relating to inheritance.',
     author = 'Neil Girdhar',
     author_email = 'mistersheik@gmail.com',
     url = 'https://github.com/NeilGirdhar/ipromise',
-    download_url = 'https://github.com/neilgirdhar/ipromise/archive/0.1.tar.gz',
+    download_url = 'https://github.com/neilgirdhar/ipromise/archive/0.2.tar.gz',
     keywords = ['testing', 'logging', 'example'], # arbitrary keywords
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -20,4 +20,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
+    python_requires='>=3.6',
 )
