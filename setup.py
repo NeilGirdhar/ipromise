8bedfe2778d5d872801fc384c2e052ef84e88f2c
Neil <mistersheik@gmail.com>
2019-09-02 19:36:29 -0400

    Polish

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import find_packages, setup
 
 setup(
     name='ipromise',
-    version='1.4',
+    version='1.5',
     description=('A Python base class that provides various decorators for '
                  'specifying promises relating to inheritance.'),
     long_description_content_type='text/x-rst',
     author='Neil Girdhar',
     author_email='mistersheik@gmail.com',
-    url='https://github.com/NeilGirdhar/ipromise',
-    download_url='https://github.com/neilgirdhar/ipromise/archive/1.4.tar.gz',
+    project_urls={
+        "Bug Tracker": "https://github.com/NeilGirdhar/ipromise/issues",
+        "Source Code": "https://github.com/NeilGirdhar/ipromise",
+    },
+    download_url="https://pypi.python.org/pypi/ipromise",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -19,10 +22,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords=['testing', 'logging', 'example'],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
